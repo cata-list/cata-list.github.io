453ae1961e99c398f2a771cd238a77108894afb9
cata-list <mwtheta@protonmail.com>
2020-05-28 17:35:11 -0700

    Minor UI updates. Removed Support & Foot components, put info in Menu. Moved Mind Field from Media/TV to YouTube. Added: 21 news articles, 1 Media/Games (Manifold Garden), meta keywords to all pages.

---
+++ _nuxt/23b81895b2571f4bedbf.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[16],{289:function(n,t,e){"use strict";e.r(t);var o={asyncData:function(n){return(0,n.redirect)("/")}},c=e(13),component=Object(c.a)(o,void 0,void 0,!1,null,null,null);t.default=component.exports}}]);
